04eebe5ae48f02630261278cd426ad5826fc51bd
yehtetaung791998@gmail.com <yehtetaung777>
2023-12-09 20:08:08 +0630

    add product array

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const products = require('./products');
 app.use(express.json());
 app.get('/', (req,res)=> {
     res.send("Welcome to our online shop ...");
 })
 
+app.get('/products', (req,res)=> {
+    res.send(products);
+})
 const port = process.env.PORT || 5000;
 
 app.listen(5000, console.log(`Server running on port 5000 ${port}`));
---
+++ backend/products.js
@@ -0,0 +1,76 @@
+const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      desc: "Description for Product 1",
+      price: 19.99,
+      image: "https://res.cloudinary.com/dklsmxlqk/image/upload/v1702128413/OnlineShop/download_q5wddd.jpg"
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      desc: "Description for Product 2",
+      price: 29.99,
+      image: "https://res.cloudinary.com/dklsmxlqk/image/upload/v1702128413/OnlineShop/download_q5wddd.jpg"
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      desc: "Description for Product 3",
+      price: 39.99,
+      image: "https://res.cloudinary.com/dklsmxlqk/image/upload/v1702128413/OnlineShop/download_q5wddd.jpg"
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      desc: "Description for Product 4",
+      price: 49.99,
+      image: "https://res.cloudinary.com/dklsmxlqk/image/upload/v1702128413/OnlineShop/download_q5wddd.jpg"
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      desc: "Description for Product 5",
+      price: 59.99,
+      image: "https://res.cloudinary.com/dklsmxlqk/image/upload/v1702128413/OnlineShop/download_q5wddd.jpg"
+    },
+    {
+      id: 6,
+      name: "Product 6",
+      desc: "Description for Product 6",
+      price: 69.99,
+      image: "https://res.cloudinary.com/dklsmxlqk/image/upload/v1702128413/OnlineShop/download_q5wddd.jpg"
+    },
+    {
+      id: 7,
+      name: "Product 7",
+      desc: "Description for Product 7",
+      price: 79.99,
+      image: "https://res.cloudinary.com/dklsmxlqk/image/upload/v1702128441/OnlineShop/images_qnw24b.jpg"
+    },
+    {
+      id: 8,
+      name: "Product 8",
+      desc: "Description for Product 8",
+      price: 89.99,
+      image: "https://res.cloudinary.com/dklsmxlqk/image/upload/v1702128441/OnlineShop/images_qnw24b.jpg"
+    },
+    {
+      id: 9,
+      name: "Product 9",
+      desc: "Description for Product 9",
+      price: 99.99,
+      image: "https://res.cloudinary.com/dklsmxlqk/image/upload/v1702128441/OnlineShop/images_qnw24b.jpg"
+    },
+    {
+      id: 10,
+      name: "Product 10",
+      desc: "Description for Product 10",
+      price: 109.99,
+      image: "https://res.cloudinary.com/dklsmxlqk/image/upload/v1702128441/OnlineShop/images_qnw24b.jpg"
+    }
+  ];
+  
+  // You can now use the 'products' array in your application
+module.exports = products;
+  
